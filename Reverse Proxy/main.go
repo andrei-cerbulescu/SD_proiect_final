d31acb4635b1a30f62db183ebfe3fe84f0e3208b
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2023-01-15 17:57:20 +0200

    no message

--- Reverse Proxy/main.go
+++ Reverse Proxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
@@ -12,6 +13,10 @@ import (
 type enrol_request struct {
 	Port    string
 	Address string
+}
+
+type reverse_proxy_request struct {
+	Request string
 }
 
 type connection struct {
@@ -38,6 +43,27 @@ func filter[T any](data []T, f func(T) bool) []T {
 	return fltd
 }
 
+func map_f[T, U any](data []T, f func(T) U) []U {
+
+	res := make([]U, 0, len(data))
+
+	for _, e := range data {
+		res = append(res, f(e))
+	}
+
+	return res
+}
+
+func send_promote_req() {
+	if len(connections) > 0 {
+		link := "http://" + connections[0].ConnectionData.Address+":"+connections[0].ConnectionData.Port + "/promoted"
+		_, e := http.Get(link)
+		if(e != nil){
+			print("uh oh")
+		}
+	}
+}
+
 func main() {
 
 	go func() {
@@ -57,8 +83,11 @@ func main() {
 			new_conn_data.Port = port
 
 			var new_con connection
-			new_con.ConnectionData = new_conn_data 
+			new_con.ConnectionData = new_conn_data
 			connections = append(connections, new_con)
+			if len(connections) == 1{
+				send_promote_req()
+			}
 
 			defer conn.Close()
 
@@ -68,17 +97,40 @@ func main() {
 				connections = filter(connections, func(e connection) bool {
 					return !(e.ConnectionData.Address == address && e.ConnectionData.Port == port)
 				})
+				send_promote_req()
 				return nil
 			})
 
 			for {
-				_, _, err := conn.ReadMessage()
+				_, data, err := conn.ReadMessage()
 				if err != nil {
 					connections = filter(connections, func(e connection) bool {
 						return !(e.ConnectionData.Address == address && e.ConnectionData.Port == port)
 					})
+					send_promote_req()
 					break
-			}
+				}
+				var reverse_proxt_req reverse_proxy_request
+				json.Unmarshal(data, &reverse_proxt_req)
+
+				switch reverse_proxt_req.Request {
+				case "get_primary":
+					if len(connections) > 0 {
+						conn.WriteMessage(1, []byte(connections[0].ConnectionData.Address+":"+connections[0].ConnectionData.Port))
+					} else {
+						conn.WriteJSON("")
+					}
+				case "get_all_secondary":
+					if len(connections) > 0 {
+						temp_conn := filter(connections, func(e connection) bool {
+							return !(e.ConnectionData.Address == connections[0].ConnectionData.Address && e.ConnectionData.Port == connections[0].ConnectionData.Port)
+						})
+						output := map_f(temp_conn, func(e connection) string {
+							return e.ConnectionData.Address + ":" + e.ConnectionData.Port
+						})
+						conn.WriteJSON(output)
+					}
+				}
 			}
 		})
 
--- Server1/index.js
+++ Server1/index.js
@@ -1,18 +1,86 @@
 const WebSocket = require('ws')
 const express = require('express')
+const bodyParser = require('body-parser');
+const axios = require('axios')
+
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var children = []
+var primary = false
+var master_address = ""
+
 const port = 3000
 const address = "localhost"
+
+var store = {}
 
 const ws = new WebSocket(`ws://localhost:8888/enrol?port=${encodeURI(port)}&address=${encodeURI(address)}`)
 ws.on('open', function open() {
   console.log('connected');
+  ws.send(JSON.stringify({
+    request: "get_primary"
+  }))
 });
+
+ws.on('message', (e) => {
+  try {
+    temp = JSON.parse(e.toString('utf8'))
+
+    children = temp
+  }
+  catch (_) {
+    master_address = e.toString('utf8')
+    if (master_address === `${address}:${port}`)
+      return
+    axios.get(`http://${master_address}/new_member?port=${encodeURI(port)}&address=${encodeURI(address)}`).then((res) => {
+      store = {
+        ...res.data
+      }
+    }).catch((err) => {
+      console.log("neok")
+    })
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/store', (req, res) => {
+  const keys = Object.keys(req.body)
+  keys.forEach(e => {
+    store[e] = req.body[e]
+  });
+  if (primary)
+    children.forEach(e => {
+      axios.post(`http://${e}/store`, req.body).then(() => {
+
+      }).catch((e) => {
+        console.log("err")
+      })
+    });
+  res.send('Success')
+})
+
+app.get('/store', (req, res) => {
+  const output = {}
+  output[req.query.obtain] = store[req.query.obtain]
+  res.send(output)
+})
+
+app.get('/new_member', (req, res) => {
+  children.push(`${req.query.address}:${req.query.port}`)
+  res.send(store)
+})
+
+
+app.get('/promoted', (req, res) => {
+  primary = true
+  console.log("This machine has been promoted!")
+  ws.send(JSON.stringify({
+    request: "get_all_secondary"
+  }))
+  res.send("Success")
 })
